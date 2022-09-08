333dd64b42102a1cdffa263fecb548c738058ca6
Kate Galati <kategalati@Kates-MacBook-Air.local>
2022-09-08 14:54:45 -0700

    add script.js, connect to index.html

---
+++ script.js
@@ -0,0 +1,7 @@
+const h2 = document.createElement("h2");
+h2.textContent = "This content added by JavaScript";
+document.querySelector("body").appendChild(h2);
+
+const p = document.createElement("p");
+p.textContent = "I am really just adding this paragraph for some additional practice when it comes to creating elements and then styling them later.";
+document.querySelector("body").appendChild(p);
